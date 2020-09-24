8464f5fcdf1f95bb966e1eebd5cd48c12cd378c1
prabhat tripathi <prabhattripathi82@gmail.com>
2020-09-24 10:42:21 +0530

    Added a commit

--- onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/OnlineBookStoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
+		// This is the main application
 		SpringApplication.run(OnlineBookStoreApplication.class, args);
 	}
 
