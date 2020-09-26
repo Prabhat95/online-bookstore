2d18c30db00a5a2bb1ba0cda5515906e37737867
prabhat tripathi <prabhattripathi82@gmail.com>
2020-09-26 19:57:58 +0530

    Created angular project and completed the feature 1

---
+++ angular-bookstore/src/app/services/book.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Book } from '../common/book';
+
+@Injectable()
+export class BookService {
+
+  private baseUrl = "http://localhost:8080/api/v1/books";
+
+  constructor(private http:HttpClient) { }
+
+  getBooks():Observable<Book[]>{
+
+    return this.http.get<GetResponseBooks>(this.baseUrl).pipe(
+      map(response=>response._embedded.books)
+
+    );
+  
+  }
+
+}
+
+interface GetResponseBooks{
+  _embedded:{
+    books:Book[];
+  }
+}
+
+
